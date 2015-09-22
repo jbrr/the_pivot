62bb0dedae90034c8add2b5c2e1fc53f128f1976
Travis Haby <travishaby@gmail.com>
2015-09-22 11:25:44 -0600

    added seed data for Candidates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Candidates.create(name: "Donald Trump", party: "Republican", bio: "Was born in the most luxurious crib the world has ever seen, in the most luxurious presidential suite in the universe. Puts his name on everything he owns, now wants to put his name on the White House.")
+
+Candidates.create(name: "Jeb Bush", party: "Republican", bio: "Born under a bush. Father sent him to live with the bush people until he was old enough to take over the family business selling shrubberies. Eventually became Governor of Florida. As president, wants to make sure that every American can afford to put a bush on their front lawn.")
+
+Candidates.create(name: "Ted Cruz", party: "Republican", bio: "Mr. Cruz was born to immigrant parents, whom he promptly reported to INS. Cruz grew up idolizing Dobby The House Elf from Harry Potter, and underwent many plastic surgeries to shape himself in Dobby's image. As President of the United States, Cruz wants to every American to be a 'Very proud wacko bird' just like him.")
+
+Candidates.create(name: "Hillary Clinton", party: "Democratic", bio: "Ms. Clinton was brought into spontaneous existence due to complex interactions between scripts on her parents' personal email server. As the pioneering robotic overlady of the human race, she (it?) strives to bridge a peaceful connection between human and robotic civilizations.")
+
+Candidates.create(name: "Bernie Sanders", party: "Democratic", bio: "Mr. Sanders is decended from a long line of Keebler elfs, but chose break off from family tradition to make sure that even America's poorest residents have a pantry full of Keebler Fudge Stripes. Legend has it that the more dishelved his appearance, the greater his power.")
+
+Candidates.create(name: "Joe Biden", party: "Democratic", bio: "While not officially running, Joe Biden's ambitions to show America 'where the party's at' are clear. Born to humble bartenders, Biden quickly endeared himself to the local patrons of 'Shotgun Joe's,' his namesake. His social acumen prompted a drunken patron one day to suggest that he run for public office. Biden, with Cheshire Cat smile, shook off his 11am buzz, and stumbled into the office of the Vice President. He looks to repeat this success in the 2016 election.")
