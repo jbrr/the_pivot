a3f682bd53b8a77307ba9c37093fb7aef47dd487
Travis Haby <travishaby@gmail.com>
2015-09-24 15:29:32 -0600

    Refactored create method of Donations Controller Moved the majority of the create logic to separate methods in the cart PORO

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,14 +1,13 @@
 class DonationsController < ApplicationController
 
   def create
-    add(session[:donations])
-    session[:donations] = raw_data
-
+    session[:donations] = cart.add(params)
     direct_to
   end
 
+  private
+  
   def direct_to
-
     candidate_id = params[:donation][:candidate_id]
     issue_id     = params[:donation][:issue_id]
 
@@ -17,21 +16,5 @@ class DonationsController < ApplicationController
     else
       redirect_to(issue_path(issue_id))
     end
-  end
-
-  def add(donations)
-    candidate_issue_id = params[:donation][:candidate_issue_id]
-    donations ||= {}
-
-    if donations[candidate_issue_id]
-      donations[candidate_issue_id] = (donations[candidate_issue_id].to_i + params[:donation][:amount].to_i).to_s
-    else
-      donations[candidate_issue_id] = params[:donation][:amount]
-    end
-    @raw_data = donations
-  end
-
-  def raw_data
-    @raw_data
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,11 +1,26 @@
 class Cart
-  def initialize(data={})
-    @data = data
+  attr_reader :raw_data
+
+  def initialize(donations={})
+    @donations = donations
   end
 
   def donations
-    @data.map do |candidate_issue_id, amount|
+    @donations.map do |candidate_issue_id, amount|
       Donation.new(candidate_issue_id: candidate_issue_id, amount: amount)
     end
+  end
+
+  def add(params)
+    candidate_issue_id = params[:donation][:candidate_issue_id]
+    donations ||= {}
+
+    if donations[candidate_issue_id]
+      donations[candidate_issue_id] = (donations[candidate_issue_id].to_i + params[:donation][:amount].to_i).to_s
+    else
+      donations[candidate_issue_id] = params[:donation][:amount]
+    end
+  
+    donations
   end
 end
