27756b1b617f7534db290314d6845f93f43c0e6e
Travis Haby <travishaby@gmail.com>
2015-09-24 15:37:30 -0600

    Further refactored logic in Donation Controller Moved candidate and issue id finder to separate methods

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -2,19 +2,24 @@ class DonationsController < ApplicationController
 
   def create
     session[:donations] = cart.add(params)
-    direct_to
+    return_to_origin_page
   end
 
   private
-  
-  def direct_to
-    candidate_id = params[:donation][:candidate_id]
-    issue_id     = params[:donation][:issue_id]
 
-    if candidate_id
-      redirect_to(candidate_path(candidate_id))
-    else
-      redirect_to(issue_path(issue_id))
+    def return_to_origin_page
+      if candidate_id
+        redirect_to(candidate_path(candidate_id))
+      else
+        redirect_to(issue_path(issue_id))
+      end
     end
-  end
+
+    def candidate_id
+      params[:donation][:candidate_id]
+    end
+
+    def issue_id
+      params[:donation][:issue_id]
+    end
 end
