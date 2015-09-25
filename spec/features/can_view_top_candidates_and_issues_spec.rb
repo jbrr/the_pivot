6b9e761048ee3494521056bfa4ec76e6040ed8e3
Bret Doucette <doucette.bret@gmail.com>
2015-09-25 16:18:57 -0600

    Implemented tests for top issues and candidates links

--- spec/features/can_view_top_candidates_and_issues_spec.rb
+++ spec/features/can_view_top_candidates_and_issues_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-feature "Can view top candidates" do
+feature "Can view top candidates and issues images" do
   scenario "as a visitor" do
     candidate = Candidate.create(name: "Donald Trump", party: "Republican", bio: "Luxurious", featured: true, last_name: "trump")
     issue = Issue.create(topic: "Gun Control", description: "Guns Guns Guns!")
@@ -32,3 +32,43 @@ feature "Can view top candidates" do
     expect(current_path).to eq("/issues/#{issue.id}")
   end
 end
+
+feature "Can view top candidates and issues links" do
+  scenario "as a visitor" do
+    candidate = Candidate.create(name: "Donald Trump", party: "Republican", bio: "Luxurious", featured: true, last_name: "trump")
+    issue = Issue.create(topic: "ISIS", description: "Guns Guns Guns!", featured: true)
+    CandidateIssue.create(candidate: candidate, issue: issue, stance: "No thanks.")
+
+    visit root_path
+    expect(current_path).to eq("/")
+
+    within("#candidate-front-title") do
+      click_link "#{candidate.name}"
+    end
+
+    expect(current_path).to eq("/candidates/#{candidate.id}")
+  end
+
+  scenario "as a visitor" do
+    candidate = Candidate.create(name: "Donald Trump", party: "Republican", bio: "Luxurious", featured: true, last_name: "trump")
+    issue = Issue.create(topic: "ISIS", description: "Guns Guns Guns!", featured: true)
+    CandidateIssue.create(candidate: candidate, issue: issue, stance: "No thanks.")
+
+    visit root_path
+    expect(current_path).to eq("/")
+
+    within("#issue-front-title") do
+      click_link "#{issue.topic}"
+    end
+
+    expect(current_path).to eq("/issues/#{issue.id}")
+  end
+end
+
+
+
+
+
+
+
+
