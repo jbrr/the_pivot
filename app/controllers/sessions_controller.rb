d9b4e91f4a6e3a95454c92708bbd6e5109fc8541
Travis Haby <travishaby@gmail.com>
2015-09-26 13:17:59 -0600

    fixed profile.2 bug for profile path apparently passing in the user object was causing it to name the path weirdly when it redirected. since its a non-restful route we didnt need to pass in the user object, the user is verified by the current_user method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:login][:username])
     if @user && @user.authenticate(params[:login][:password])
       session[:user_id] = @user.id
-      redirect_to profile_path(@user)
+      redirect_to profile_path
     else
       flash[:error] = "Invalid Login"
       render :new
