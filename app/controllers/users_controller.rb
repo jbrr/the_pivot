06b28c9cc097a150b557d23947134bbcf4cf6a64
Bret Doucette <doucette.bret@gmail.com>
2015-09-26 15:49:36 -0600

    Fixed Bug with creating a new user profile url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to profile_path(@user)
+      redirect_to profile_path
     else
       flash[:errors] = @user.errors.full_messages.join(", ")
       render :new
