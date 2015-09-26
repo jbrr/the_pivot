5e2b8d3e831c93a45e89a4ab9fdec0d873bcac24
Travis Haby <travishaby@gmail.com>
2015-09-26 12:30:57 -0600

    added candidate issues for jeb, ted and hillz

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,9 +22,54 @@ Issue.create(topic: "Gun Control", description: "'Let them have guns!,' some pol
 
 Issue.create(topic: "Economy", description: "The hard working, and deserving, 1 percenters are the way of the future according to some. Others envision a single world commune where everyone's opinion is equally important.", featured: true)
 
+CandidateIssue.create(stance: "Only the most luxuriously dressed grooms/brides will be allowed to wed each other.", candidate_id: 1, issue_id: 1)
+
 CandidateIssue.create(stance: "All trees should be gilded.", candidate_id: 1, issue_id: 2)
 
+CandidateIssue.create(stance: "We're gonna make a giant sea wall to keep out all the illegal Chinese, and we're gonna make Mexico pay for it!", candidate_id: 1, issue_id: 3)
+
 CandidateIssue.create(stance: "Kill 'em all!", candidate_id: 1, issue_id: 4)
+
+CandidateIssue.create(stance: "Gold guns for everyone, with diamond bullets.", candidate_id: 1, issue_id: 5)
+
+CandidateIssue.create(stance: "I'll run America like one my many, many, many, many successful bankrupt businesses.", candidate_id: 1, issue_id: 6)
+
+CandidateIssue.create(stance: "Only homosexual and bicurious shrubberies will be permitteed the right to marry the plant they love.", candidate_id: 2, issue_id: 1)
+
+CandidateIssue.create(stance: "Global warming puts all of us in danger, but most importantly the indigenous bush people. Haliburton will be given a no-bid multi-billion dollar contract to protect the bush people's dwinding habitat and general livelihood.", candidate_id: 2, issue_id: 2)
+
+CandidateIssue.create(stance: "Latin American bushes will be allowed to seek asylum at nurseries around the nation.", candidate_id: 2, issue_id: 3)
+
+CandidateIssue.create(stance: "We will earn favor with ISIS by welcoming them to the neighborhood with well manicured bushes. Either that or nuke them?", candidate_id: 2, issue_id: 4)
+
+CandidateIssue.create(stance: "All guns will be banned from use for US Citizens inside and outside our nation. The gratuitous and horrific murder of Cecil the bush has raied awareness to our serious gun problem and made action on this issue a cornerstone of my platform.", candidate_id: 2, issue_id: 5)
+
+CandidateIssue.create(stance: "I will measure the strength of our economy by whether all Americans have the opportunity to provide thier family with numerous bushes and shrubberies from the nursery of thie choice.", candidate_id: 2, issue_id: 6)
+
+CandidateIssue.create(stance: "Only straight men and women deserve the 50% chance of becoming devorced after marriage, just like God intended.", candidate_id: 3, issue_id: 1)
+
+CandidateIssue.create(stance: "We will bend Mother Nature to our will. I'm thinking of something analagous to the evil corporation from James Cameron's 'Avatar.'", candidate_id: 3, issue_id: 2)
+
+CandidateIssue.create(stance: "Now that I made it here in America, no one else is allowed in. That's fair right?", candidate_id: 3, issue_id: 3)
+
+CandidateIssue.create(stance: "Our young, brave American soldiers are ready and willing to take on ISIS, an existential threat to our nation. Just not my kids. Or my friends' kids.", candidate_id: 3, issue_id: 4)
+
+CandidateIssue.create(stance: "Why does rambo get to have all the fun?", candidate_id: 3, issue_id: 5)
+
+CandidateIssue.create(stance: "Our economy does best when government gets out of the way. That's why we need to cut all funding for building public roads and public schools to bring America back to greatness.", candidate_id: 3, issue_id: 6)
+
+
+CandidateIssue.create(stance: "All Americans deserve to have a husband that embarasses them on national television multiple times.", candidate_id: 4, issue_id: 1)
+
+CandidateIssue.create(stance: "The precedent of imminent domain will be used to relocate the residents of Alaska elsewhere to make way for the worlds largest polar bear sanctuary.", candidate_id: 4, issue_id: 2)
+
+CandidateIssue.create(stance: "All undocumented immigrants should be given voting rights so the demographics shift in my favor and ensure a second term as president.", candidate_id: 4, issue_id: 3)
+
+CandidateIssue.create(stance: "I plan to continue my predecssor's campagin of mostly indescriminate drone attacks.", candidate_id: 4, issue_id: 4)
+
+CandidateIssue.create(stance: "I plan to implement a Chris Rock-inspired policy on firearms: guns are free but bullets cost $10,000 each.", candidate_id: 4, issue_id: 5)
+
+CandidateIssue.create(stance: "To ensure a strong economy we need to invest in our public schools. All Americans should feel good about sending their kids to our nation's public institution. Except Chelsea. I'll be damned if my grandchildren go to school with the proletariat.", candidate_id: 4, issue_id: 6)
 
 CandidateIssue.create(stance: "The human race is a plague on our planet, extermination is the only reasonable course of action.", candidate_id: 5, issue_id: 2)
 
