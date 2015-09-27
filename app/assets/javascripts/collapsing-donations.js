2365db23e8bdec6edf85815566dd9dc7b1370c82
roseak <rose.kohn@gmail.com>
2015-09-26 18:09:04 -0600

    Reinstated javascript file.

---
+++ app/assets/javascripts/collapsing-donations.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".donation-time").hide()
+  $(".donation-candidate").on("click", function(event){
+    $(this).siblings().first().toggle();
+    $("span", this).toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
+  })
+})
