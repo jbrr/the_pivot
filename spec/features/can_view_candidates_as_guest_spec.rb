4edd17a8a97eb3240edd52130035541dfb898595
Travis Haby <travishaby@gmail.com>
2015-09-27 15:57:38 -0600

    i swear to god hound if you talk back one more time...

--- spec/features/can_view_candidates_as_guest_spec.rb
+++ spec/features/can_view_candidates_as_guest_spec.rb
@@ -4,7 +4,7 @@ feature "Can view candidates" do
   scenario "guest can see candidates list" do
     Candidate.create(name: "Donald Trump",
                     party: "Republican",
-                    bio: "Luxurious")
+                      bio: "Luxurious")
 
     visit root_path
     expect(current_path).to eq("/")
@@ -21,16 +21,16 @@ feature "Can view candidates" do
   scenario "guest can see candidates unique pictures" do
     Candidate.create(name: "Donald Trump",
                     party: "Republican",
-                    bio: "Luxurious",
-                    last_name: "trump")
+                      bio: "Luxurious",
+                last_name: "trump")
     Candidate.create(name: "Joe Biden",
                     party: "Democratic",
-                    bio: "Whiskey",
-                    last_name: "biden")
+                      bio: "Whiskey",
+                last_name: "biden")
     Candidate.create(name: "Ted Cruz",
                     party: "Republican",
-                    bio: "Religion",
-                    last_name: "cruz")
+                      bio: "Religion",
+                last_name: "cruz")
 
     visit candidates_path
 
