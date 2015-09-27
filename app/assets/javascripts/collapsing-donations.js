20f3c411749a04331462a10b22fc3c1fdf98f60a
roseak <rose.kohn@gmail.com>
2015-09-27 09:08:49 -0600

    Added hover color for stances headers, and added work around for jquery because of turbolinks.

--- app/assets/javascripts/collapsing-donations.js
+++ app/assets/javascripts/collapsing-donations.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
-  $(".donation-time").hide()
-  $(".donation-candidate").on("click", function(event){
-    $(this).siblings().first().toggle();
-    $("span", this).toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
+var ready;
+ready = function() {
+
+  $(document).ready(function(){
+    $(".donation-time").hide()
+    $(".donation-candidate").on("click", function(event){
+      $(this).siblings().first().toggle();
+      $("span", this).toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
+    })
   })
-})
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
