a4c9f0bab40c52edd569888a7808559de8f0ab3c
roseak <rose.kohn@gmail.com>
2015-09-27 13:06:08 -0600

    Added more detail to tests for featured candidates and issues.  Added id and style for issues.

--- spec/features/can_view_top_candidates_and_issues_spec.rb
+++ spec/features/can_view_top_candidates_and_issues_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-feature "Can view top candidates and issues images" do
+feature "Can view and use top candidates and issues images" do
   before do
     candidate = Candidate.create(name: "Donald Trump", party: "Republican", bio: "Luxurious", featured: true, last_name: "trump")
     issue = Issue.create(topic: "Isis", description: "Guns Guns Guns!", featured: true)
@@ -8,11 +8,23 @@ feature "Can view top candidates and issues images" do
   end
 
   scenario "as a visitor" do
-    candidate = Candidate.find_by(name: "Donald Trump")
-
     visit root_path
     expect(current_path).to eq("/")
 
+    expect(page).to have_content("Welcome to Earmarked")
+    expect(page).to have_content("Top Candidates")
+    within("#candidates") do
+      expect(page).to have_content("Donald Trump")
+    end
+    within("#issues") do
+      expect(page).to have_content("ISIS")
+    end
+  end
+
+  scenario "can visit a specific featured candidate from their image"
+    candidate = Candidate.find_by(name: "Donald Trump")
+
+    visit root_path
     within("#candidate-#{candidate.id}") do
       find(:xpath, "//a/img[@alt='Trump']/..").click
     end
@@ -20,33 +32,21 @@ feature "Can view top candidates and issues images" do
     expect(current_path).to eq("/candidates/#{candidate.id}")
   end
 
-  scenario "as a visitor" do
+  scenario "can visit a specific featured issue from its image" do
     issue = Issue.find_by(topic: "Isis")
 
     visit root_path
-    expect(current_path).to eq("/")
-
     within("#issue-#{issue.id}") do
       find(:xpath, "//a/img[@alt='Isis']/..").click
     end
 
     expect(current_path).to eq("/issues/#{issue.id}")
   end
-end
 
-feature "Can view top candidates and issues links" do
-  before do
-    candidate = Candidate.create(name: "Donald Trump", party: "Republican", bio: "Luxurious", featured: true, last_name: "trump")
-    issue = Issue.create(topic: "Isis", description: "Guns Guns Guns!", featured: true)
-    candidate_issue = CandidateIssue.create(candidate: candidate, issue: issue, stance: "No thanks.")
-  end
-
-  scenario "as a visitor" do
+  scenario "can visit a specific featured candidate from its name" do
     candidate = Candidate.find_by(name: "Donald Trump")
 
     visit root_path
-    expect(current_path).to eq("/")
-
     within("#candidate-front-title") do
       click_link "#{candidate.name}"
     end
@@ -54,12 +54,10 @@ feature "Can view top candidates and issues links" do
     expect(current_path).to eq("/candidates/#{candidate.id}")
   end
 
-  scenario "as a visitor" do
+  scenario "can visit a specific featured issue from its name" do
     issue = Issue.find_by(topic: "Isis")
 
     visit root_path
-    expect(current_path).to eq("/")
-
     within("#issue-front-title") do
       click_link "#{issue.topic}"
     end
@@ -67,11 +65,3 @@ feature "Can view top candidates and issues links" do
     expect(current_path).to eq("/issues/#{issue.id}")
   end
 end
-
-
-
-
-
-
-
-
