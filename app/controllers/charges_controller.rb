a1b6b3a2251a6bedf8df17a0662c7549e89940bb
Mimi Schatz <mcschatz@gmail.com>
2015-09-30 17:24:08 -0600

    fixed some hound issues in the charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,31 +5,31 @@ class ChargesController < ApplicationController
   end
 
   def create
-      @amount = (cart.total * 100)
+    @amount = (cart.total * 100)
 
-      customer = Stripe::Customer.create(
-        :email => 'example@stripe.com',
-        :card  => params[:stripeToken]
-      )
+    customer = Stripe::Customer.create(
+      :email => "example@stripe.com",
+      :card  => params[:stripeToken]
+    )
 
-      charge = Stripe::Charge.create(
-        :customer    => customer.id,
-        :amount      => @amount,
-        :description => 'Rails Stripe customer',
-        :currency    => 'usd'
-      )
+    Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => "Rails Stripe customer",
+      :currency    => "usd"
+    )
 
-      order = Crusher.create_order(session, current_user, cart)
-      session[:donations].clear
-      if current_user.phone_number
-        SendNotification.new.text_message(current_user.phone_number, order, current_user)
-      end
+    order = Crusher.create_order(session, current_user, cart)
+    session[:donations].clear
+    if current_user.phone_number
+      SendNotification.new.text_message(current_user.phone_number, order, current_user)
+    end
 
-      redirect_to order_path(order)
+    redirect_to order_path(order)
 
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to charges_path
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
   end
 end
 
