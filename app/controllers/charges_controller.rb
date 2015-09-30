635a38100f1ba69400bb862da28abe41db1d9b3f
Mimi Schatz <mcschatz@gmail.com>
2015-09-30 17:06:31 -0600

    removed Pay with Card button from non-logged in user's page. Only logged in users can checkout. Guests are now asked to Join from the cart page before donating.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,26 +5,27 @@ class ChargesController < ApplicationController
   end
 
   def create
-    @amount = (cart.total * 100)
+      @amount = (cart.total * 100)
 
-    customer = Stripe::Customer.create(
-      :email => 'example@stripe.com',
-      :card  => params[:stripeToken]
-    )
+      customer = Stripe::Customer.create(
+        :email => 'example@stripe.com',
+        :card  => params[:stripeToken]
+      )
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Rails Stripe customer',
+        :currency    => 'usd'
+      )
 
-    order = Crusher.create_order(session, current_user, cart)
-    session[:donations].clear
-    if current_user.phone_number
-      SendNotification.new.text_message(current_user.phone_number, order, current_user)
-    end
-    redirect_to order_path(order)
+      order = Crusher.create_order(session, current_user, cart)
+      session[:donations].clear
+      if current_user.phone_number
+        SendNotification.new.text_message(current_user.phone_number, order, current_user)
+      end
+
+      redirect_to order_path(order)
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
@@ -34,17 +35,12 @@ end
 
 class Crusher
   def self.create_order(session, current_user, cart)
-    if current_user
-      order = Order.create(user_id: current_user.id, total: cart.total)
-      cart.donations.each do |donation|
-        donation.order_id = order.id
-        donation.user_id  = current_user.id
-        donation.save
-      end
-      order
-    else
-      flash[:errors] = "Please sign in to donate!"
-      redirect_to cart_path
+    order = Order.create(user_id: current_user.id, total: cart.total)
+    cart.donations.each do |donation|
+      donation.order_id = order.id
+      donation.user_id  = current_user.id
+      donation.save
     end
+    order
   end
 end
