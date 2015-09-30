c74940454eee7f8c9cb170fde61ff6a5f0210f2f
Bret Doucette <doucette.bret@gmail.com>
2015-09-30 10:12:58 -0600

    Added glcy to error and success messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,13 +8,18 @@ module ApplicationHelper
     { success: "alert-success", errors: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
   end
 
+  def bootstrap_icon_for flash_type
+    { success: "ok", errors: "remove-circle", alert: "warning-sign", notice: "exclamation-sign" }[flash_type.to_sym] || "question-sign"
+  end
+
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, sanitize(message), class: "alert #{bootstrap_class_for(msg_type)} fade in") do
-              concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
-              concat message
-            end)
-    end
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+        concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+        concat content_tag(:i, nil, class: "glyphicon glyphicon-#{bootstrap_icon_for(msg_type)}")
+        concat message
+        end)
+      end
     nil
   end
 end
