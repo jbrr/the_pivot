13531b48a5ba01e91c5af46c88a9e74fb7dc8b8c
Bret Doucette <doucette.bret@gmail.com>
2015-09-30 11:12:00 -0600

    Add number and presence validations for order

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  let(:user) {
+    User.create(first_name: "Donald", last_name: "Trump", email: "trump@luxury.com", username: "therealtrump", password: "password", password_confirmation: "password")
+  }
+
+  let(:order) {
+     Order.create(total: 420, user_id: user.id, status: "completed")
+  }
+
+  it "is valid" do
+    expect(order).to be_valid
+  end
+
+  it "is invalid without a total" do
+    order.total = nil
+    expect(order).to be_invalid
+  end
+
+  it "is invalid without a user_id" do
+    order.user_id = nil
+    expect(order).to be_invalid
+  end
+
+  it "is invalid without status" do
+    order.status = nil
+    expect(order).to be_invalid
+  end
+
+  it "must have an integer amount" do
+    order.total = "himom"
+    expect(order).to be_invalid
+  end
+
+
+end
