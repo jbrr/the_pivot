d4b2de994085cc050ac3dd84332bbe1b3d4d68fd
Bret Doucette <doucette.bret@gmail.com>
2015-09-30 10:47:15 -0600

    Push ar logic to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ActiveRecord::Base
   validates :email, email_format: { message: " is not in a valid format."}
   validates :password, length: { minimum: 8, message: "must have a minimum of 8 characters"}
   validates :password_confirmation, presence: true
+
+
+  def self.completed(id)
+    Order.where("user_id = ? AND status = ? ", id, "completed")
+  end
+
+  def self.pending(id)
+    @orders_pending = Order.where("user_id = ? AND status = ? ", id, "pending")
+  end
+
+  def self.cancelled(id)
+    @orders_cancelled = Order.where("user_id = ? AND status = ? ", id, "cancelled")
+  end
 end
