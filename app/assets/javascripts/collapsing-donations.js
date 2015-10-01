cb3ff1edf9eda25624175ee612b06ab93b344c6c
roseak <rose.kohn@gmail.com>
2015-10-01 11:59:45 -0600

    Added plus and minus glyphicons to the toggle.

--- app/assets/javascripts/collapsing-donations.js
+++ app/assets/javascripts/collapsing-donations.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   $(".donation-time").hide();
   $(".donation-candidate").on("click", function(event){
     $(this).siblings().first().toggle();
-    $("span", this).toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
+    $("span", this).toggleClass("glyphicon-plus-sign glyphicon-minus-sign");
   });
 });
-
