d0d8b00cc75c96ede57dfe9b1e3298e2dca899bb
Travis Haby <travishaby@gmail.com>
2015-10-01 11:12:33 -0600

    refactored created_at information to method called on order object in views

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,8 @@ class Order < ActiveRecord::Base
 
   validates :total, :user_id, :status, presence: true
   validates :total, numericality: { only_integer: true }
+
+  def when_created
+    created_at.strftime("%A, %B %d, %Y at %I:%M%p")
+  end
 end
