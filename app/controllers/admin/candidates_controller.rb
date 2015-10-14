bf6bcdf8e65bf21cb16ad5b7bb8d2a0693f78484
selfup <boudinot.regis@yahoo.com>
2015-10-13 20:20:43 -0600

    Successfully extracted stances from candidate-issues by candidate

--- app/controllers/admin/candidates_controller.rb
+++ app/controllers/admin/candidates_controller.rb
@@ -1,12 +1,20 @@
 class Admin::CandidatesController < ApplicationController
   def show
     @candidate = Candidate.find(params[:id])
-    @issues = CandidateIssue.find(params[:id])
+    @candidate_issues = CandidateIssue.where(candidate_id: params[:id]).to_a
+    @stances = @candidate_issues.map { |ci| ci.stance }
+    require 'pry' ; binding.pry
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def candidate_id
+
   end
 end
