fcd5ad3f838a78c82479c8901e75951ccbb24ae2
selfup <boudinot.regis@yahoo.com>
2015-10-18 16:05:38 -0600

    Can create campaign managers, need to refactor and change the views a bit

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -16,8 +16,12 @@ class Admin::DashboardsController < ApplicationController
     end
 
     def create
+      # @current_user = current_user
       @user = User.new(new_campaign_manager_params)
       if @user.save
+        role = Role.find_by(name: "campaign_manager")
+        candidate_id = current_candidate.id
+        @user.user_roles << UserRole.create(user_id: @user.id, role_id: role.id, candidate_id: candidate_id)
         redirect_to admin_candidate_path(current_candidate.slug)
       else
         render :new
@@ -33,7 +37,6 @@ class Admin::DashboardsController < ApplicationController
     def new_campaign_manager_params
       params.require(:user).permit(:first_name, :last_name, :email,
                                    :phone_number, :username, :password,
-                                   :password_confirmation, :candidate_id,
-                                   :role_id)
+                                   :password_confirmation)
     end
 end
