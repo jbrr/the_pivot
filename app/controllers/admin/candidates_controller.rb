b3583a0fdb5f53ed7e36525ab02d59ce0500c6a1
roseak <rose.kohn@gmail.com>
2015-10-18 12:33:43 -0600

    Added callback to redirect to correct slug for campaign managers.

--- app/controllers/admin/candidates_controller.rb
+++ app/controllers/admin/candidates_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CandidatesController < ApplicationController
-  before_action :set_candidate
+  before_action :set_candidate, :set_slug
 
   def show
   end
@@ -15,5 +15,11 @@ class Admin::CandidatesController < ApplicationController
   def set_candidate
     cid                = current_user.user_roles.first.candidate_id
     @current_candidate = Candidate.find_by(id: cid)
+  end
+
+  def set_slug
+    if Candidate.find_by(slug: params[:candidate]) != @current_candidate
+      redirect_to admin_candidate_path(@current_candidate.slug)
+    end
   end
 end
