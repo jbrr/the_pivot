30b33cb3a65d817031fe783b793f53df6e567907
roseak <rose.kohn@gmail.com>
2015-10-18 19:11:05 -0600

    Added progress bar for total campaign goal on the candidate dash and fixed the donation to be saved at 2700 or less.

--- app/helpers/admin/candidates_helper.rb
+++ app/helpers/admin/candidates_helper.rb
@@ -10,4 +10,21 @@ module Admin::CandidatesHelper
     goals.reduce(:+)
   end
 
+  def campaign_raised_total
+    @current_candidate.candidate_issues.inject(0) {|sum, x| sum + candidate_issue_raised_total(x)}
+  end
+
+  def campaign_percent_calculation
+    campaign_raised_total.to_f / total_goal.to_f * 100
+  end
+
+  def campaign_progress_bar_percentage
+    if campaign_percent_calculation.nan?
+      0
+    elsif campaign_percent_calculation > 100
+      100
+    else
+      campaign_percent_calculation
+    end
+  end
 end
