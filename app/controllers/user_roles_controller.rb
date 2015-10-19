39e0c3cab65461df9d13e74195cd25b1b6ef4d70
selfup <boudinot.regis@yahoo.com>
2015-10-18 18:45:45 -0600

    Full function for admin decline and approve, must add extra auth for managers to not see admin dashboard

--- app/controllers/user_roles_controller.rb
+++ app/controllers/user_roles_controller.rb
@@ -3,7 +3,6 @@ class UserRolesController < ApplicationController
   end
 
   def update
-    require 'pry' ; binding.pry
     user_role = UserRole.find_by(user_id: current_user.id)
     user_role.update(user_role_params)
     redirect_to candidate_path(params[:user_role][:candidate_id])
