e77c1fdfbf5a3655c67074df5c9dc3bc86c16206
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-20 13:57:05 -0600

    Defined needed local variables in candidate issue controller

--- app/controllers/candidate_issues_controller.rb
+++ app/controllers/candidate_issues_controller.rb
@@ -4,10 +4,10 @@ class CandidateIssuesController < ApplicationController
     candidate_issue_candidate
     if candidate_issue.update(candidate_issue_params)
       flash[:success] = "Stance Successfully Updated"
-      redirect_to edit_admin_candidate_path(candidate: candidate.slug)
+      redirect_to edit_admin_candidate_path(candidate: candidate_issue_candidate.slug)
     else
       flash[:errors] = "Do you need a tissue for you issue??"
-      redirect_to edit_admin_candidate_path(candidate: candidate.slug)
+      redirect_to edit_admin_candidate_path(candidate: candidate_issue_candidate.slug)
     end
   end
 
@@ -17,8 +17,11 @@ class CandidateIssuesController < ApplicationController
     params.require(:candidate_issue).permit(:stance, :goal)
   end
 
+  def candidate_issue
+    CandidateIssue.find(params[:id])
+  end
+
   def candidate_issue_candidate
-    candidate_issue = CandidateIssue.find(params[:id])
-    candidate = candidate_issue.candidate
+    candidate_issue.candidate
   end
 end
