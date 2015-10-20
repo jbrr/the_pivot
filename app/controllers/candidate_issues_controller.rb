5671e09f2e47585d994106c1b05caea46c34d3bb
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-20 14:00:37 -0600

    Removed extraneous code from candidate issue controller

--- app/controllers/candidate_issues_controller.rb
+++ app/controllers/candidate_issues_controller.rb
@@ -1,13 +1,12 @@
 class CandidateIssuesController < ApplicationController
 
   def update
-    candidate_issue_candidate
     if candidate_issue.update(candidate_issue_params)
       flash[:success] = "Stance Successfully Updated"
-      redirect_to edit_admin_candidate_path(candidate: candidate_issue_candidate.slug)
+      redirect_to edit_admin_candidate_path(candidate: candidate_issue.candidate.slug)
     else
       flash[:errors] = "Do you need a tissue for you issue??"
-      redirect_to edit_admin_candidate_path(candidate: candidate_issue_candidate.slug)
+      redirect_to edit_admin_candidate_path(candidate: candidate_issue.candidate.slug)
     end
   end
 
@@ -19,9 +18,5 @@ class CandidateIssuesController < ApplicationController
 
   def candidate_issue
     CandidateIssue.find(params[:id])
-  end
-
-  def candidate_issue_candidate
-    candidate_issue.candidate
   end
 end
