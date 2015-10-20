1f8877315f75f3c510d978677712c964efb40457
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-20 16:44:12 -0600

    Add donation day method

--- app/models/donation_date.rb
+++ app/models/donation_date.rb
@@ -5,4 +5,8 @@ class DonationDate
     @current_candidate = current_candidate
     @donations = current_candidate.donations
   end
+
+  def donation_day
+    @donations.map { |donation| donation.created_at.strftime("%B %d %Y") }
+  end
 end
