5d4017b8ec7541b6e030fe9f50428461aa55d8f0
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-21 16:29:27 -0600

    Add method to populate pie chart

--- app/models/donation_chart.rb
+++ app/models/donation_chart.rb
@@ -5,6 +5,15 @@ class DonationChart
       parsed_date = donation.created_at.strftime("%s").to_i * 1000
       donation_date << ["new Date(#{parsed_date})", donation.amount]
     end
-    donation_date = donation_date.to_s.gsub('"','')
+    donation_date = donation_date.to_s.gsub('"', '')
+  end
+
+  def self.donation_by_candidate_issue(candidate)
+    donation_candidate_issue = []
+    candidate.donations.map do |donation|
+      donation_topic = donation.issue.topic
+      donation_candidate_issue << ["#{donation_topic}", donation.amount]
+    end
+    donation_candidate_issue = donation_candidate_issue.to_s.gsub('"', '')
   end
 end
