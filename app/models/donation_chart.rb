494da25e8600af2a0e6cb4a2b0bcb70fc164c325
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-21 19:17:19 -0600

    Best method ever

--- app/models/donation_chart.rb
+++ app/models/donation_chart.rb
@@ -1,4 +1,4 @@
-class DonationChart
+class DonationChart < ActiveRecord::Base
   def self.donation_by_date(candidate)
     donation_date = []
     candidate.donations.map do |donation|
@@ -8,12 +8,25 @@ class DonationChart
     donation_date = donation_date.to_s.gsub('"', '')
   end
 
+  def self.donation_issues
+    Issue.all.map {|issue| issue.topic }
+  end
+
+  def self.issue_donation_total
+    Issue.all.map do |issue|
+      issue.candidate_issues.map { |candidate_issue| candidate_issue.donations.
+        map { |donation| donation.amount }}.reduce(:+).reduce(:+)
+    end.map do |num|
+      if num.nil?
+        0
+      else
+        num
+      end
+    end  
+  end
+
   def self.donations_by_issue(candidate)
     donation_candidate_issue = []
-    candidate.donations.map do |donation|
-      donation_topic = donation.issue.topic
-      donation_candidate_issue << ["#{donation_topic}", donation.amount]
-    end
-    donation_candidate_issue
+    donation_issues.zip(issue_donation_total)
   end
 end
