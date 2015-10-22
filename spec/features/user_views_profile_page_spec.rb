84663c64921ef4754a1b75e03f86ed58fdc83888
selfup <boudinot.regis@yahoo.com>
2015-10-22 09:22:41 -0600

    all tests pass

--- spec/features/user_views_profile_page_spec.rb
+++ spec/features/user_views_profile_page_spec.rb
@@ -51,7 +51,7 @@ feature "Logged in user views profile page" do
   scenario "can visit profile via navbar link" do
     user = User.find_by(username: "therealtrump")
     within("#navbar") do
-      click_link "Therealtrump"
+      click_link "Donald"
     end
 
     expect(current_path).to eq(profile_path)
